21ce168d4b16badb028380fb8b18ab0b5286ba90
RebecaCamilo <rebecacamilo3@gmail.com>
2024-01-11 18:59:41 -0300

    feat: add WorkedTime domain object

---
+++ src/main/java/com/example/timerecorder/model/WorkedTime.java
@@ -0,0 +1,14 @@
+package com.example.timerecorder.model;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class WorkedTime {
+
+    private Long id;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Duration duration;
+    private String description;
+
+}
