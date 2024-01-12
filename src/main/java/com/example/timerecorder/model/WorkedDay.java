01fe48f25021a9510628ff92cdb29eb7ffa1a524
RebecaCamilo <rebecacamilo3@gmail.com>
2024-01-11 21:36:04 -0300

    feat: add List of WorkedTime in WorkedDay object

--- src/main/java/com/example/timerecorder/model/WorkedDay.java
+++ src/main/java/com/example/timerecorder/model/WorkedDay.java
@@ -1,10 +1,12 @@
 package com.example.timerecorder.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class WorkedDay {
 
     private Long id;
     private LocalDate data;
+    private List<WorkedTime> workedTimes;
 
 }
